f71042bc56dbe30cd1080072fb55df4f2e2105d5
Andrew Jerrim <Andrew.Jerrim@gmail.com>
2018-01-21 16:48:22 +1100

    Added TestProcessor class

--- Source/TestProcessor.cpp
+++ Source/TestProcessor.cpp
@@ -9,3 +9,147 @@
 */
 
 #include "TestProcessor.h"
+
+TestProcessor::TestProcessor()
+    : currentSpec ()
+{ }
+void TestProcessor::prepare (const dsp::ProcessSpec& spec)
+{
+    currentSpec = spec;
+
+    const auto start = Time::getMillisecondCounterHiRes();
+
+// =====================
+    prepareToBeTested(spec);
+// =====================
+
+    const auto duration = Time::getMillisecondCounterHiRes() - start;
+    if (duration<prepDurationMin) prepDurationMin = duration;
+    if (duration>prepDurationMax) prepDurationMax = duration;
+    prepDurationSum += duration;
+    prepDurationCount++;
+
+    // Reset process duration statistics as these will not be valid if the spec has changed
+    procDurationMin = 0.0;
+    procDurationMax = 0.0;
+    procDurationSum = 0.0;
+    procDurationCount = 0.0;
+}
+void TestProcessor::process (const dsp::ProcessContextReplacing<float>& context)
+{
+    const auto start = Time::getMillisecondCounterHiRes();
+    
+// =====================
+    processToBeTested (context);
+// =====================
+
+    const auto duration = Time::getMillisecondCounterHiRes() - start;
+    if (duration<procDurationMin) procDurationMin = duration;
+    if (duration>procDurationMax) procDurationMax = duration;
+    procDurationSum += duration;
+    procDurationCount++;
+}
+void TestProcessor::reset ()
+{
+    const auto start = Time::getMillisecondCounterHiRes();
+
+// =====================
+    resetToBeTested();
+// =====================
+
+    const auto duration = Time::getMillisecondCounterHiRes() - start;
+    if (duration<resetDurationMin) resetDurationMin = duration;
+    if (duration>resetDurationMax) resetDurationMax = duration;
+    resetDurationSum += duration;
+    resetDurationCount++;
+}
+dsp::ProcessSpec TestProcessor::getCurrentProcessSpec () const
+{
+    return currentSpec;
+}
+double TestProcessor::queryPrepareDurationAverage () const
+{
+    return prepDurationSum / prepDurationCount;
+}
+double TestProcessor::queryPrepareDurationMax () const
+{
+    return prepDurationMax;
+}
+double TestProcessor::queryPrepareDurationMin() const
+{
+    return prepDurationMin;
+}
+double TestProcessor::queryPrepareDurationNumSamples () const
+{
+    return prepDurationCount;
+}
+double TestProcessor::queryProcessingDurationAverage () const
+{
+    return procDurationSum / procDurationCount;
+}
+double TestProcessor::queryProcessingDurationMax () const
+{
+    return procDurationMax;
+}
+double TestProcessor::queryProcessingDurationMin() const
+{
+    return procDurationMin;
+}
+double TestProcessor::queryProcessingDurationNumSamples () const
+{
+    return procDurationCount;
+}
+double TestProcessor::queryResetDurationAverage () const
+{
+    return resetDurationSum / resetDurationCount;
+}
+double TestProcessor::queryResetDurationMax () const
+{
+    return resetDurationMax;
+}
+double TestProcessor::queryResetDurationMin() const
+{
+    return resetDurationMin;
+}
+double TestProcessor::queryResetDurationNumSamples () const
+{
+    return resetDurationCount;
+}
+void TestProcessor::resetStatistics ()
+{
+    prepDurationMin = 1.0E100;
+    prepDurationMax = -1.0;
+    prepDurationSum = 0.0;
+    prepDurationCount = 0.0;
+    
+    procDurationMin = 1.0E100;
+    procDurationMax = -1.0;
+    procDurationSum = 0.0;
+    procDurationCount = 0.0;
+
+    resetDurationMin = 1.0E100;
+    resetDurationMax = -1.0;
+    resetDurationSum = 0.0;
+    resetDurationCount = 0.0;
+}
+
+
+bool is_sse_aligned (const float* data)
+{
+    return (reinterpret_cast<uintptr_t>(data) & 0xF) == 0;
+}
+
+String get_buffer_alignment_status (AudioBuffer<float>& buffer)
+{
+    auto bufIsAligned = true;
+	for (auto ch = 0; ch < buffer.getNumChannels(); ++ch)
+		bufIsAligned = bufIsAligned && is_sse_aligned (buffer.getArrayOfReadPointers ()[ch]);
+
+	String alignmentMsg = "AudioSampleBuffer should ";
+	if (bufIsAligned)
+		alignmentMsg = "AudioSampleBuffer is aligned";
+	else
+		alignmentMsg = "AudioSampleBuffer is not aligned";
+
+	return alignmentMsg;
+}
--- Source/TestProcessor.h
+++ Source/TestProcessor.h
@@ -9,3 +9,78 @@
 */
 
 #pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+/** Inherit from this and implement the virtual methods in order to create a customised TestProcessor.
+*/
+class TestProcessor : public dsp::ProcessorBase
+{
+public:
+	
+    TestProcessor();
+	~TestProcessor() = default;
+
+    /** Wrapper method which surrounds the code you want to test */
+    void prepare (const dsp::ProcessSpec& spec) override;
+    /** Wrapper method which surrounds the code you want to test */
+    void process (const dsp::ProcessContextReplacing<float>& context) override;
+    /** Wrapper method which surrounds the code you want to test */
+    void reset() override;
+
+    dsp::ProcessSpec getCurrentProcessSpec() const;
+
+    /** Returns the average time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryPrepareDurationAverage() const;
+    /** Returns the maximum time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryPrepareDurationMax() const;
+    /** Returns the minimum time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryPrepareDurationMin() const;
+    /** Returns the number of times processToBeTested() has been called since statistics were reset. */
+    double queryPrepareDurationNumSamples() const;
+
+    /** Returns the average time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryProcessingDurationAverage() const;
+    /** Returns the maximum time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryProcessingDurationMax() const;
+    /** Returns the minimum time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryProcessingDurationMin() const;
+    /** Returns the number of times processToBeTested() has been called since statistics were reset. */
+    double queryProcessingDurationNumSamples() const;
+
+    /** Returns the average time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryResetDurationAverage() const;
+    /** Returns the maximum time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryResetDurationMax() const;
+    /** Returns the minimum time it takes to run processToBeTested() at the current ProcessSpec. */
+    double queryResetDurationMin() const;
+    /** Returns the number of times processToBeTested() has been called since statistics were reset. */
+    double queryResetDurationNumSamples() const;
+
+    /** Reset statistics */
+    void resetStatistics();
+
+protected:
+
+    /** This will be called by the wrapper in order to perform the necessary tests. */
+    virtual void prepareToBeTested (const dsp::ProcessSpec& spec) = 0;
+    /** This will be called by the wrapper in order to perform the necessary tests. */
+    virtual void processToBeTested (const dsp::ProcessContextReplacing<float>& context) = 0;
+    /** This will be called by the wrapper in order to perform the necessary tests. */
+    virtual void resetToBeTested() = 0;
+
+private:
+    	
+    dsp::ProcessSpec currentSpec;
+    double prepDurationMin = 1.0E100, prepDurationMax = -1.0, prepDurationSum = 0.0, prepDurationCount = 0.0;
+    double procDurationMin = 1.0E100, procDurationMax = -1.0, procDurationSum = 0.0, procDurationCount = 0.0;
+    double resetDurationMin = 1.0E100, resetDurationMax = -1.0, resetDurationSum = 0.0, resetDurationCount = 0.0;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TestProcessor);
+};
+
+/** Returns true if the specified pointer points to 16 byte aligned data. */
+static inline bool is_sse_aligned (const float* data);
+
+/** Returns a string describing the buffer alignment status. */
+static String get_buffer_alignment_status (AudioBuffer<float>& buffer);
