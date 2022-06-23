500d061c98e1f3a8ca299cb8b0116bc2856f4f04
Andrew Jerrim <AndrewJJ@users.noreply.github.com>
2022-06-24 00:08:20 +1000

    Version bump to 1.1.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -50,7 +50,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "DSP Testbench";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.1.0";
+    const int          versionNumber  = 0x10100;
 }
 #endif
